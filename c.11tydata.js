e1a9bab1b15fd9d1f886a6c3af79928299156e63
Evan Boehs <evan@boehs.org>
2022-03-03 01:16:15 -0500

    basic backlink support

---
+++ c.11tydata.js
@@ -0,0 +1,39 @@
+// This regex finds all wikilinks in a string
+const wikilinkRegExp = /\[\[([^|]+?)(\|([\s\S]+?))?\]\]/g
+
+function caselessCompare(a, b) {
+    return a.toLowerCase() === b.toLowerCase();
+}
+
+module.exports = {
+    layout: "post.njk",
+    eleventyComputed: {
+        backlinks: (data) => {
+            const notes = data.collections.all;
+            const currentFileSlug = data.page.filePathStem.replace('/pages/c/', '');
+            let backlinks = [];
+
+            // Search the other notes for backlinks
+            for(const otherNote of notes) {
+                const noteContent = otherNote.template.frontMatter.content;
+                // Get all links from otherNote
+                const outboundLinks = (noteContent.match(wikilinkRegExp) || [])
+                    .map(link => (
+                        // Extract link location
+                        link.slice(2,-2)
+                            .split("|")[0]
+                            .replace(/.(md|markdown)\s?$/i, "")
+                            .trim()
+                    ));
+                // If the other note links here, return related info
+                if(outboundLinks.some(link => caselessCompare(link, currentFileSlug))) {
+                    backlinks.push({
+                        url: otherNote.url,
+                        title: otherNote.data.title,
+                    })
+                }
+            }
+            return backlinks;
+        }
+    }
+}
